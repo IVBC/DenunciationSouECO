14025dc431ecda20a2b34b5da3947f76dc02c6d7
STRNeoh <strneoh@localhost.localdomain>
2020-02-16 18:09:14 -0400

    update denunciation module

--- src/app/pages/search-denunciation/search-denunciation.module.ts
+++ src/app/pages/search-denunciation/search-denunciation.module.ts
@@ -13,6 +13,16 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatIconModule} from '@angular/material/icon';
 import { Zipcode } from '../shared/pipes/zipcodePipe';
 import { Phone } from '../shared/pipes/phonePipe';
+import { TableModule } from "primeng/table";
+import { SliderModule } from 'primeng/slider';
+import { DialogModule } from 'primeng/dialog';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { InputTextModule } from 'primeng/inputtext';
+import { TabViewModule } from 'primeng/tabview';
 
 
 
@@ -35,7 +45,17 @@ import { Phone } from '../shared/pipes/phonePipe';
     ReactiveFormsModule,
     FormsModule,
     MatTooltipModule,
-    MatIconModule
+    MatIconModule,
+    TableModule,
+		SliderModule,
+		DialogModule,
+		MultiSelectModule,
+		ContextMenuModule,
+		DropdownModule,
+		ButtonModule,
+		ToastModule,
+		InputTextModule,
+		TabViewModule,
 
 
     // MatLabel
