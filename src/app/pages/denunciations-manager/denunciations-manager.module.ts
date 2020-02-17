d17cce6776fed2521d83dc94690da2192bef12e6
isaquevilson <isaque.vilson27@gmail.com>
2020-02-17 13:12:23 -0400

    adding head only manager

--- src/app/pages/denunciations-manager/denunciations-manager.module.ts
+++ src/app/pages/denunciations-manager/denunciations-manager.module.ts
@@ -23,10 +23,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
 import {MatSelectModule} from '@angular/material/select';
+import { DenunciationsManagerComponent } from './denunciations-manager.component';
 
 
 @NgModule({
-  declarations: [DenunciationListComponent, DenunciationFormComponent],
+  declarations: [DenunciationListComponent, DenunciationFormComponent, DenunciationsManagerComponent],
   imports: [
     CommonModule,
     DenunciationsManagerRoutingModule,
