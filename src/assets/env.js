02b1fb4852051e4e128e19ca371e4bac1b984b6b
STRNEOH <strneoh@localhost.localdomain>
2020-07-20 16:44:05 -0300

    adjustment responsiveness

--- src/assets/env.js
+++ src/assets/env.js
@@ -3,6 +3,6 @@
 
   // Environment variables
   // window["env"]["apiUrl"] = "https://soueco-api.tce.am.gov.br"; 206.189.194.249
-  window["env"]["apiUrl"] = "http://localhost:3333";
+  window["env"]["apiUrl"] = "http://206.189.194.249";
   window["env"]["debug"] = true;
 })(this);
