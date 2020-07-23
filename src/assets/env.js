9e287b310989f93e1adaff1e06eada8a1dd44e21
Isaque Vilson <ivbc.eng17@uea.edu.br>
2020-07-22 21:48:41 -0400

    fix: adding env

--- src/assets/env.js
+++ src/assets/env.js
@@ -3,6 +3,7 @@
 
   // Environment variables
   // window["env"]["apiUrl"] = "https://soueco-api.tce.am.gov.br"; 206.189.194.249
-  window["env"]["apiUrl"] = "http://localhost:3333";
+  window["env"]["apiUrl"] = "https://soueco-api.tce.am.gov.br";
+  // window["env"]["apiUrl"] = "http://localhost:3333";
   window["env"]["debug"] = true;
 })(this);
